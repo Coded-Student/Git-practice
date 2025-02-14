3a986929683b7a0012ecce70c60a8d6c984ef4e0
Gabi <abigail.m.morales11@gmail.com>
2025-02-14 15:29:03 +0800

    add elif statement

--- If-else.py
+++ If-else.py
@@ -1,10 +1,16 @@
 username = input("Enter username: ")
+password = input("Enter a password: ")
 
-if username == "":
-    print("Please enter a username.")
+if username == "" and password == "" :
+    print("Please enter a username and password.")
     print("")
     username = input("Enter username: ")
+    password = input("Enter password: ")
 
+elif username == username and not password :
+    print(f"Incorrect password, please try again.")
+    print("")
+    password = input("Enter password: ")
 
 else:
     print(f"Hello, {username}")
